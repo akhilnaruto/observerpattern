c34c055d5950f309f480e262b23eb3c568c9f77a
Akhil <akhil.munna02@gmail.com>
2015-07-12 22:54:44 +0530

    Adding Missed file..

---
+++ ObserverPattern/ObserverPattern/WeatherData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class WeatherData : ThirdPartyWeatherAPI
+    {
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay();
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay();
+        ForecastDisplay forecastDisplay = new ForecastDisplay();
+        public void MeasurementsChanged()
+        {
+            float temp = getTemperature();
+            float humidity = getHumidity();
+            float pressure = getPressure();
+
+            currentConditionsDisplay.Update(temp, humidity, pressure);
+            statisticsDisplay.Update(temp, humidity, pressure);
+            forecastDisplay.Update(temp, humidity, pressure);
+
+        }
+    }
+}
